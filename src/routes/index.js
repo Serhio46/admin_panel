82081211c36388ed7e36adbd2abdfab7c35a9153
siarhei <siarhei@wsa-135-72b.icx.local>
2022-03-24 13:57:01 +0300

    add: added products routes to app

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,15 +7,22 @@ import Single from "../pages/single/Single";
 export const routesName =  {
     'HOME': '/',
     'LOGIN': '/login',
-    'LIST': '/list',
-    'NEW': '/new',
-    'SINGLE': '/single'
+    'USERS': '/users',
+    'NEW_USER': '/users/new',
+    'SINGL_EUSER': '/users/:userId',
+    'PRODUCTS': '/products',
+    'SINGLE_PRODUCT': '/products/:productId',
+    'NEW_PRODUCT': '/products/new'
+
 };
 
 export const publicRoutes = [
     { 'path': routesName.HOME, 'component': Home },
     { 'path': routesName.LOGIN, 'component': Login },
-    { 'path': routesName.LIST, 'component': List },
-    { 'path': routesName.NEW, 'component': New },
-    { 'path': routesName.SINGLE, 'component': Single },
+    { 'path': routesName.USERS, 'component': List },
+    { 'path': routesName.NEW_USER, 'component': New },
+    { 'path': routesName.SINGL_EUSER, 'component': Single },
+    { 'path': routesName.PRODUCTS, 'component': List },
+    { 'path': routesName.SINGLE_PRODUCT, 'component': Single },
+    { 'path': routesName.NEW_PRODUCT, 'component': New },
  ];
