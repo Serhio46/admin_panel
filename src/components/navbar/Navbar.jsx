788cf48fbf9f01334ab26d438818258dafcf2c43
siarhei <siarhei@wsa-135-72b.icx.local>
2022-03-24 16:07:53 +0300

    feat: added nav-bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,51 @@
-import classes from './navbar.module.scss'
+import classes from './navbar.module.scss';
+
+import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
+import LanguageIcon from '@mui/icons-material/Language';
+import NightlightRoundTwoToneIcon from '@mui/icons-material/NightlightRoundTwoTone';
+import FullscreenExitTwoToneIcon from '@mui/icons-material/FullscreenExitTwoTone';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ListIcon from '@mui/icons-material/List';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import Avatar from '@mui/material/Avatar';
+import Badge from '@mui/material/Badge';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className={classes.navbar}>
+            <div className={classes.wrapper}>
+                <div className={classes.search}>
+                    <input type="text" placeholder='Search...' />
+                    <SearchTwoToneIcon />
+                </div>
+                <div className={classes.icons}>
+                    <div className={classes.iconsItem}>
+                        <LanguageIcon />
+                        English
+                    </div>
+                    <div className={classes.iconsItem}>
+                        <NightlightRoundTwoToneIcon />
+                    </div>
+                    <div className={classes.iconsItem}>
+                        <FullscreenExitTwoToneIcon />
+                    </div>
+                    <div className={classes.iconsItem}>
+                         <Badge badgeContent={3} color="error">
+                            <NotificationsNoneIcon />
+                         </Badge>
+                    </div>
+                    <div className={classes.iconsItem}>
+                        <Badge badgeContent={2} color="error">
+                            <ChatBubbleOutlineIcon />
+                        </Badge>
+                    </div>
+                    <div className={classes.iconsItem}>
+                        <ListIcon />
+                    </div>
+                    <Avatar className={classes.avatar}/>
+                </div>
+            </div>    
+        </div>
     );
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';  
 import classes from './home.module.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar'
 
 const Home = () => {
     return (
         <div className={classes.home}>
             <Sidebar />
-            <div className={classes.container}>container</div>
+            <div className={classes.container}>
+                <Navbar />
+                container
+            </div>
         </div>
         
     );
